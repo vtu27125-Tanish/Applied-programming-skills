4fddfeebf84908185b5c9eb26a77b0eecaf30532
vtu27125-Tanish <vtu27125@veltech.edu.in>
2026-03-25 10:33:08 +0530

    Time: 1 ms (6.49%), Space: 43.3 MB (43.58%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+
+            // Go to leftmost node
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+
+            curr = stack.pop();
+            result.add(curr.val);
+
+            curr = curr.right;
+        }
+
+        return result;
+    }
+}
